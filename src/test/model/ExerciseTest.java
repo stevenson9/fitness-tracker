50545ee29c8b71f09be35e5b6f200431a18af50c
stevenson9 <sdson9@gmail.com>
2023-02-25 00:28:11 -0800

    ExerciseTest made

---
+++ src/test/model/ExerciseTest.java
@@ -0,0 +1,26 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExerciseTest {
+    private Exercise testExercise;
+
+    @BeforeEach
+    void runBefore() {
+        testExercise = new Exercise("Bench Press", 5, 5, 225);
+
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("Bench Press", testExercise.getName());
+        assertEquals(5, testExercise.getReps());
+        assertEquals(5, testExercise.getSets());
+        assertEquals(225, testExercise.getWeight());
+    }
+
+
+}
